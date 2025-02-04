188062ed8f7af08d3f5d88d205901bcec15e111f
Alexander Pruss <arpruss@gmail.com>
2025-02-03 21:44:41 -0600

    better watch

--- watch.py
+++ watch.py
@@ -6,20 +6,27 @@ import subprocess
 
 PDF_VIEWER = "C:/Users/alexander_pruss/AppData/Local/SumatraPDF/SumatraPDF.exe"
 EDITOR = "c:/program files/notepad++/notepad++.exe"
+DIRECTORY = "."
+MAIN_FILE = "document"
+TEMP_FILE = "__temp__" + MAIN_FILE
 
 class MyHandler(FileSystemEventHandler):
     def on_modified(self, event):
         if not event.is_directory and event.src_path.endswith(".tex"):
             print("touched "+event.src_path)
-            subprocess.Popen(["pdflatex", "-interaction=nonstopmode", "document"])
-            #os.system("pdflatex document")
+            try:
+                subprocess.run(["pdflatex", "-interaction=nonstopmode", "-jobname=" + TEMP_FILE, MAIN_FILE])
+                os.unlink(MAIN_FILE + ".pdf")
+                os.rename(TEMP_FILE + ".pdf", MAIN_FILE + ".pdf")
+            except subprocess.CalledProcessError:
+                print("error!")
 
 # Create observer and event handler
 observer = Observer()
 event_handler = MyHandler()
 
 # Set up observer to watch a specific directory
-directory_to_watch = "."
+directory_to_watch = DIRECTORY
 observer.schedule(event_handler, directory_to_watch, recursive=True)
 
 # Start the observer
